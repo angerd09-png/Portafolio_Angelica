8bd988ef792ae58390d839defb7198e107f6315e
angerd09-png <angerd09@gmail.com>
2026-07-28 22:28:17 -0500

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,6 @@ export default function Nav() {
     <nav className="nav">
       <a href="#hero" className="nav__brand">Angelica Ruiz</a>
       <div className="nav__links">
-        <a href="#sobre-mi">Sobre mí</a>
         <a href="#proyectos">Proyectos</a>
         <a href="#contacto">Contacto</a>
       </div>
